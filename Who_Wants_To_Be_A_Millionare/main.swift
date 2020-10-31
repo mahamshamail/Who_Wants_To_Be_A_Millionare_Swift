cdbf51f0eb690b4f7726204fe9fff789dc958074
maham <mahamshamail@gmail.com>
2020-10-31 05:25:09 -0400

    Initial Commit

---
+++ Who_Wants_To_Be_A_Millionare/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Who_Wants_To_Be_A_Millionare
+//
+//  Created by Maham Shamail on 31/10/2020.
+//  Copyright © 2020 Maham Shamail. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
